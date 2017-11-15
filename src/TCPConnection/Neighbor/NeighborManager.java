f49d4be6bd14c68f696b5027d8542ab2db979f2f
Tyovaish <tyovaish13@gmail.com>
2017-11-14 23:15:09 -0500

    Improved Neighbor manager to incorporate times

--- src/TCPConnection/Neighbor/NeighborManager.java
+++ src/TCPConnection/Neighbor/NeighborManager.java
@@ -1,5 +1,6 @@
 package TCPConnection.Neighbor;
 
+import File.CommonFileParser;
 import TCPConnection.TCPConnection;
 
 import java.util.ArrayList;
@@ -9,18 +10,33 @@ import java.util.ArrayList;
  */
 public class NeighborManager implements Runnable {
     ArrayList<TCPConnection> neighborConnections;
+    long unchokingInterval;
+    long optimisticallyUnchokingInterval;
     public NeighborManager(){
+        unchokingInterval= CommonFileParser.getUnchokingInterval()*1000;
+        optimisticallyUnchokingInterval=CommonFileParser.getOptimisticUnchokingInterval()*1000;
         neighborConnections=new ArrayList<TCPConnection>();
     }
     @Override
     public void run() {
-        long startTime=System.currentTimeMillis();
+        long unchokingStartTime=System.currentTimeMillis();
+        long optmisticallyUnchokeTime=System.currentTimeMillis();
         while(true){
-            if(System.currentTimeMillis()-startTime>1000){
-                System.out.println("Been a Seconds");
-                startTime=System.currentTimeMillis();
+            if(System.currentTimeMillis()-unchokingStartTime>unchokingInterval){
+                System.out.println("Unchoke");
+                if(neighborConnections.size()!=0){
+                    System.out.println("Hello");
+                }
+                unchokingStartTime=System.currentTimeMillis();
+            }
+            if(System.currentTimeMillis()-optmisticallyUnchokeTime>optimisticallyUnchokingInterval){
+                System.out.println("OptimisticallyUnchoke");
+                optmisticallyUnchokeTime=System.currentTimeMillis();
             }
         }
     }
+    public synchronized void addNeighbor(TCPConnection tcpConnection){
 
+        neighborConnections.add(tcpConnection);
+    }
 }
